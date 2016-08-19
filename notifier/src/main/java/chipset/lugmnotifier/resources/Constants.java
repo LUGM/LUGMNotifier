d9b4cf0008bbb966d9333ea47be0387ec07ba4e0
chipset95 <chipset95@yahoo.co.in>
2016-08-19 09:38:04 +0530

    Migrate to back4apps.com

--- notifier/src/main/java/chipset/lugmnotifier/resources/Constants.java
+++ notifier/src/main/java/chipset/lugmnotifier/resources/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
     public static String KEY_CLASS_NOTIFICATION = "Notifications";
     public static String KEY_DETAIL = "detail";
     public static String KEY_SHOW = "show";
-    public static String URL_GITHUB = "https://github.com/chipset95/LUGMNotifier";
+    public static String URL_GITHUB = "https://github.com/LUGM/LUGMNotifier";
     public static String URL_PLAY_STORE = "https://play.google.com/store/apps/details?id=chipset.lugmnotifier";
 
     public static String APP_VERSION = BuildConfig.VERSION_NAME;
